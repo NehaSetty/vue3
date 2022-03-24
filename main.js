4edcb1fc08f77b7824a15c11cf0e4e3491915187
Neha Setty <nvs@mouseflow.com>
2022-03-24 14:55:16 +0100

    Add v-bind property

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: "Socks"
+            product: "Socks",
+            description: "There are two pairs of socks available",
+            image: "./assets/images/socks_blue.jpg"
         }
     }
 })
