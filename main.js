aff8a779906543d82df78043c1e26a0ff0953e5a
Neha Setty <nvs@mouseflow.com>
2022-03-24 15:50:46 +0100

    Add conditional rendering

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Socks",
-            description: "There are two pairs of socks available",
+            description: "There are two colors of socks available",
             image: "./assets/images/socks_blue.jpg",
-            url: "https://vuejs.org/guide/introduction.html"
+            url: "https://vuejs.org/guide/introduction.html",
+            available: true,
+            onSale: 100,
+
         }
     }
 })
